433394b88d404aebcdd562abfa3ee755f243bb18
iamBedant <bebi2527@live.in>
2016-04-16 20:27:06 +0530

    Initializing Content Provider

--- app/src/main/java/com/iambedant/nanodegree/quickbite/data/local/persistent/DataDbHelper.java
+++ app/src/main/java/com/iambedant/nanodegree/quickbite/data/local/persistent/DataDbHelper.java
@@ -1,7 +1,53 @@
 package com.iambedant.nanodegree.quickbite.data.local.persistent;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Kuliza-193 on 4/16/2016.
  */
-public class DataDbHelper {
+public class DataDbHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+
+    static final String DATABASE_NAME = "quickbite.db";
+
+    public DataDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        final String SQL_CREATE_CUISINE_TABLE = "CREATE TABLE " + DataContract.CuisinesEntry.TABLE_NAME + " (" +
+                DataContract.CuisinesEntry._ID + " INTEGER PRIMARY KEY," +
+                DataContract.CuisinesEntry.COLUMN_CUISINE_NAME + " TEXT UNIQUE NOT NULL, " +
+                DataContract.CuisinesEntry.COLUMN_CUISINE_ID + " TEXT NOT NULL, " +
+                DataContract.CuisinesEntry.COLUMN_IS_FAVOURITE + "  INTEGER NOT NULL, " +
+                " );";
+
+        final String SQL_CREATE_RESTAURANT_TABLE = "CREATE TABLE " + DataContract.RestaurantEntry.TABLE_NAME + " (" +
+                DataContract.RestaurantEntry._ID + " INTEGER PRIMARY KEY," +
+                DataContract.RestaurantEntry.COLUMN_RESTAURANT_ID + " TEXT UNIQUE NOT NULL, " +
+                DataContract.RestaurantEntry.COLUMN_RESTAURANT_NAME + " TEXT NOT NULL, " +
+                DataContract.RestaurantEntry.COLUMN_RESTAURANT_COVER_IMAGE + "  TEXT NOT NULL, " +
+                DataContract.RestaurantEntry.COLUMN_RESTAURANT_LAT + " REAL NOT NULL, " +
+                DataContract.RestaurantEntry.COLUMN_RESTAURANT_LONG + " REAL NOT NULL " +
+                DataContract.RestaurantEntry.COLUMN_RESTAURANT_CUISINE + " REAL NOT NULL " +
+                DataContract.RestaurantEntry.COLUMN_RESTAURANT_RATINGE + " REAL NOT NULL " +
+                DataContract.RestaurantEntry.COLUMN_RESTAURANT_PRICE + "  INTEGER NOT NULL, " +
+                " );";
+
+
+        db.execSQL(SQL_CREATE_CUISINE_TABLE);
+        db.execSQL(SQL_CREATE_RESTAURANT_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + DataContract.RestaurantEntry.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + DataContract.CuisinesEntry.TABLE_NAME);
+        onCreate(db);
+    }
 }
--- app/src/main/java/com/iambedant/nanodegree/quickbite/data/local/persistent/DataProvider.java
+++ app/src/main/java/com/iambedant/nanodegree/quickbite/data/local/persistent/DataProvider.java
@@ -1,7 +1,77 @@
 package com.iambedant.nanodegree.quickbite.data.local.persistent;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
 /**
  * Created by Kuliza-193 on 4/16/2016.
  */
-public class DataProvider {
+public class DataProvider extends ContentProvider{
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private DataDbHelper mOpenHelper;
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new DataDbHelper(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+        return super.bulkInsert(uri, values);
+    }
+
+
+
+
+    static UriMatcher buildUriMatcher() {
+        // I know what you're thinking.  Why create a UriMatcher when you can use regular
+        // expressions instead?  Because you're not crazy, that's why.
+
+        // All paths added to the UriMatcher have a corresponding code to return when a match is
+        // found.  The code passed into the constructor represents the code to return for the root
+        // URI.  It's common to use NO_MATCH as the code for this case.
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = DataContract.CONTENT_AUTHORITY;
+
+        // For each type of URI you want to add, create a corresponding code.
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER, WEATHER);
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER + "/*", WEATHER_WITH_LOCATION);
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER + "/*/#", WEATHER_WITH_LOCATION_AND_DATE);
+
+        matcher.addURI(authority, WeatherContract.PATH_LOCATION, LOCATION);
+        return matcher;
+    }
 }
